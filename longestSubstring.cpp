4fd552a6797a908988ad1957ead1bfe6d5caf5cc
Parth Anand <parthanand428@gmail.com>
2026-04-30 14:12:57 +0530

    Added new DSA problems solutions

--- longestSubstring.cpp
+++ longestSubstring.cpp
@@ -1,24 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// class Solution{
+// public:
+//     int longestSubstring(string& s){
+//         int n = s.size();
+//         int maxLength = 0;
+
+//         for(int i = 0; i < n; i++){
+//             vector<int> Hash(256, 0);
+
+//             for(int j = i; j < n; j++){
+//                 if(Hash[s[j]] == 1) break;
+
+//                 int len = j - i + 1;
+//                 maxLength = max(maxLength, len);
+//                 Hash[s[j]] = 1;
+//             }
+//         }
+//         return maxLength;
+//     }
+// };
+
 class Solution{
 public:
     int longestSubstring(string& s){
         int n = s.size();
-        int maxLength = 0;
+        vector<int> Hash(256, -1);
+        int l=0, r=0, maxLen = 0;
 
-        for(int i = 0; i < n; i++){
-            vector<int> Hash(256, 0);
-
-            for(int j = i; j < n; j++){
-                if(Hash[s[j]] == 1) break;
-
-                int len = j - i + 1;
-                maxLength = max(maxLength, len);
-                Hash[s[j]] = 1;
+        while(r<n){
+            if(Hash[s[r]] != -1){
+                if(Hash[s[r]] >= l){
+                    l = Hash[s[r]] + 1;
+                }
             }
+            int len = r - l + 1;
+            maxLen = max(maxLen, len);
+            Hash[s[r]] = r;
+            r++;
         }
-        return maxLength;
+        return maxLen;
     }
 };
 
